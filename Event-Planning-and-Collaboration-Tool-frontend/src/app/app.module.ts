f7c76126a3776b27c3abf7b4cb6bd9c370b58984
Stefan Velev <sdvelev@gmail.com>
2023-06-21 15:25:01 +0300

    feat: add necessary modules

--- Event-Planning-and-Collaboration-Tool-frontend/src/app/app.module.ts
+++ Event-Planning-and-Collaboration-Tool-frontend/src/app/app.module.ts
@@ -7,18 +7,52 @@ import {MaterialUIModule} from "./material-ui/material-ui.module";
 import { EventComponent } from './event/event.component';
 import {MatMenuModule} from "@angular/material/menu";
 import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './header/header.component';
+import { NavLoggedInComponent } from './navigation/nav-logged-in/nav-logged-in.component';
+import { NavLoggedOutComponent } from './navigation/nav-logged-out/nav-logged-out.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './content/home/home.component';
+import {RouterOutlet} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+import { SectionHomeComponent } from './content/section/home/section-home/section-home.component';
+import {HttpClientModule} from "@angular/common/http";
+import { NavComponent } from './navigation/nav/nav.component';
+import { EventsComponent } from './content/events/events.component';
+import { SectionEventsComponent } from './content/section/events/section-events/section-events.component';
+import { EventCardsComponent } from './content/section/events/event-cards/event-cards.component';
+import { FooterComponent } from './footer/footer.component';
+import { BudgetsComponent } from './content/budgets/budgets.component';
+import { SectionBudgetsComponent } from './content/section/budgets/section-budgets/section-budgets.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventComponent,
-    HomepageComponent
+    HomepageComponent,
+    HeaderComponent,
+    NavLoggedInComponent,
+    NavLoggedOutComponent,
+    LoginComponent,
+    RegistrationComponent,
+    HomeComponent,
+    SectionHomeComponent,
+    NavComponent,
+    EventsComponent,
+    SectionEventsComponent,
+    EventCardsComponent,
+    FooterComponent,
+    BudgetsComponent,
+    SectionBudgetsComponent
   ],
   imports: [
     BrowserModule,
     MaterialUIModule,
     MatCardModule,
-    MatMenuModule
+    MatMenuModule,
+    RouterOutlet,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
