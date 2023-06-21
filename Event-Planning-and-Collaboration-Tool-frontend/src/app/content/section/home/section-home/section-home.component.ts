83dace0384af15549d3640cc79289f41ee936664
Stefan Velev <sdvelev@gmail.com>
2023-06-21 15:20:59 +0300

    feat: add initial version of home section

---
+++ Event-Planning-and-Collaboration-Tool-frontend/src/app/content/section/home/section-home/section-home.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+import {PlannedEvent} from "../../../../models/planned-event";
+import {EventService} from "../../../../services/event.service";
+
+@Component({
+  selector: 'app-section-home',
+  templateUrl: './section-home.component.html',
+  styleUrls: ['./section-home.component.css']
+})
+export class SectionHomeComponent{
+
+
+}
