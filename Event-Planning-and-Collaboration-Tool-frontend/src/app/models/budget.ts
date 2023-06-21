9acda8d54ea43cb2d8b094e8c1882813fe291d8c
Stefan Velev <sdvelev@gmail.com>
2023-06-21 15:23:58 +0300

    feat: add budgets model

---
+++ Event-Planning-and-Collaboration-Tool-frontend/src/app/models/budget.ts
@@ -0,0 +1,13 @@
+export class Budget {
+  id: number;
+  description: string;
+  expenditure_category: string;
+  amount: number;
+
+  constructor(id: number = 0, description: string = "", expenditure_category: string = "", amount: number = 0) {
+    this.id = id;
+    this.description = description;
+    this.expenditure_category = expenditure_category;
+    this.amount = amount;
+  }
+}
