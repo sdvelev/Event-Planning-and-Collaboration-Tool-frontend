6b7264e35b9f424405bead532bf3a8a1332c1f9c
Stefan Velev <sdvelev@gmail.com>
2023-06-26 11:00:17 +0300

    refactor: added new fields to budget model

--- Event-Planning-and-Collaboration-Tool-frontend/src/app/models/budget.ts
+++ Event-Planning-and-Collaboration-Tool-frontend/src/app/models/budget.ts
@@ -3,11 +3,15 @@ export class Budget {
   description: string;
   expenditure_category: string;
   amount: number;
+  show_expenses: boolean;
+  event_id: number;
 
-  constructor(id: number = 0, description: string = "", expenditure_category: string = "", amount: number = 0) {
+  constructor(id: number = 0, description: string = "", expenditure_category: string = "", amount: number = 0, show_expenses: boolean = false, event_id: number = 0) {
     this.id = id;
     this.description = description;
     this.expenditure_category = expenditure_category;
     this.amount = amount;
+    this.show_expenses = show_expenses;
+    this.event_id = event_id;
   }
 }
