07006dd24e5ff0cef8800084f487d35ded091995
Stefan Velev <sdvelev@gmail.com>
2023-06-26 11:01:05 +0300

    feat: added more methods in budget service

--- Event-Planning-and-Collaboration-Tool-frontend/src/app/services/budget.service.ts
+++ Event-Planning-and-Collaboration-Tool-frontend/src/app/services/budget.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, tap} from "rxjs";
-import {PlannedEvent} from "../models/planned-event";
+import {BehaviorSubject, Observable, tap} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {Budget} from "../models/budget";
 
@@ -14,6 +13,32 @@ export class BudgetService {
   constructor(private httpClient: HttpClient) { }
 
   getBudgetsByEventId(id: number) {
-    return this.httpClient.get<Budget[]>('http://localhost:8080/budgets?assigned_event_id=' + id);
+    return this.httpClient.get<Budget[]>('http://localhost:8080/budgets/search?event_id=' + id);
+  }
+
+  getAllBudgets() {
+    return this.httpClient.get<Budget[]>('http://localhost:8080/budgets').toPromise();
+  }
+
+  getBudgetById(id: number) {
+    return this.httpClient.get<Budget>('http://localhost:8080/budgets/search?id=' + id)
+      .pipe(
+        tap((budget: Budget) => {
+          this.budget$.next(budget);
+        })
+      );
+  }
+
+  addBudget(budgetDto: Budget, eventId: number): Observable<number> {
+    console.log(budgetDto);
+    return this.httpClient.post<number>(`http://localhost:8080/budgets?assigned_event_id=${eventId}`, budgetDto);
+  }
+
+  editBudget(budgetId: number, budgetDto: Budget): Observable<boolean> {
+    return this.httpClient.put<boolean>(`http://localhost:8080/budgets/set?budget_id=${budgetId}`, budgetDto);
+  }
+
+  deleteBudget(budgetId: number, eventId: number): Observable<boolean> {
+    return this.httpClient.delete<boolean>(`http://localhost:8080/budgets?id=${budgetId}&assigned_event_id=${eventId}`);
   }
 }
