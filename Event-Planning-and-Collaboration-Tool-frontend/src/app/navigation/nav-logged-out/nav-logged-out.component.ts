2e7a122e1224e4a8d474d64716c50352464053f6
Stefan Velev <sdvelev@gmail.com>
2023-06-29 23:44:44 +0300

    refactor: updated navigations code relating to authentication

--- Event-Planning-and-Collaboration-Tool-frontend/src/app/navigation/nav-logged-out/nav-logged-out.component.ts
+++ Event-Planning-and-Collaboration-Tool-frontend/src/app/navigation/nav-logged-out/nav-logged-out.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EventFormComponent} from "../../content/section/events/event-form/event-form.component";
+import {PlannedEvent} from "../../models/planned-event";
+import {EventService} from "../../services/event.service";
+import {MatDialog} from "@angular/material/dialog";
+import {LoginComponent} from "../../login/login.component";
+import {RegistrationComponent} from "../../registration/registration.component";
 
 @Component({
   selector: 'app-nav-logged-out',
   templateUrl: './nav-logged-out.component.html',
   styleUrls: ['./nav-logged-out.component.css']
 })
-export class NavLoggedOutComponent {
+export class NavLoggedOutComponent implements OnInit{
 
+  // plannedEvents$!: Promise<PlannedEvent[] | undefined>;
+
+  constructor(private dialog: MatDialog) {
+  }
+
+  ngOnInit(): void {
+    // this.plannedEvents$ = this.eventService.getAllEvents();
+  }
+
+  openLoginForm() {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: '600px',
+      height: '320px',
+      data: null
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        window.location.reload();
+      }
+    });
+  }
+
+  openRegistrationForm() {
+    const dialogRef = this.dialog.open(RegistrationComponent, {
+      width: '600px',
+      height: '740px',
+      data: null
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        window.location.reload();
+      }
+    });
+  }
 }
--- Event-Planning-and-Collaboration-Tool-frontend/src/app/navigation/nav/nav.component.ts
+++ Event-Planning-and-Collaboration-Tool-frontend/src/app/navigation/nav/nav.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
-  isLogged: boolean = true;
+export class NavComponent implements OnInit {
+  isLogged: boolean = false;
+
+  ngOnInit(): void {
+    this.checkToken();
+  }
+
+  private checkToken(): void {
+    const token = localStorage.getItem('tokenEventCrafter');
+    this.isLogged = !!token;
+  }
 }
