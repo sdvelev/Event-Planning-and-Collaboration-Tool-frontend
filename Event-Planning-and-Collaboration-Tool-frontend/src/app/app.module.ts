a440fa9f4c417af976ce042bf12cb4da1520a058
Stefan Velev <sdvelev@gmail.com>
2023-06-29 23:55:36 +0300

    refactor: updated AppModule with the latest components

--- Event-Planning-and-Collaboration-Tool-frontend/src/app/app.module.ts
+++ Event-Planning-and-Collaboration-Tool-frontend/src/app/app.module.ts
@@ -47,6 +47,12 @@ import { BudgetFormComponent } from './content/section/budgets/budget-form/budge
 import { BudgetEditFormComponent } from './content/section/budgets/budget-edit-form/budget-edit-form.component';
 import { ExpenseFormComponent } from './content/section/budgets/expense-form/expense-form.component';
 import { ExpenseEditFormComponent } from './content/section/budgets/expense-edit-form/expense-edit-form.component';
+import { SectionUserComponent } from './content/section/home/section-user/section-user.component';
+import { UserCardComponent } from './content/section/home/user-card/user-card.component';
+import { ChangeUsernameFormComponent } from './content/section/home/change-username-form/change-username-form.component';
+import { ChangePasswordFormComponent } from './content/section/home/change-password-form/change-password-form.component';
+import { EditUserFormComponent } from './content/section/home/edit-user-form/edit-user-form.component';
+import { DeleteUserFormComponent } from './content/section/home/delete-user-form/delete-user-form.component';
 
 @NgModule({
   declarations: [
@@ -85,7 +91,13 @@ import { ExpenseEditFormComponent } from './content/section/budgets/expense-edit
     BudgetFormComponent,
     BudgetEditFormComponent,
     ExpenseFormComponent,
-    ExpenseEditFormComponent
+    ExpenseEditFormComponent,
+    SectionUserComponent,
+    UserCardComponent,
+    ChangeUsernameFormComponent,
+    ChangePasswordFormComponent,
+    EditUserFormComponent,
+    DeleteUserFormComponent
   ],
   imports: [
     BrowserModule,
