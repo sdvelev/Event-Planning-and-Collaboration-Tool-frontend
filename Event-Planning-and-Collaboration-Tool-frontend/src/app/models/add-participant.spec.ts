93d033aa71b3cce6b99baa5fc942d7aaa156f91a
Stefan Velev <sdvelev@gmail.com>
2023-06-30 13:01:34 +0300

    feat: added model for adding participant

---
+++ Event-Planning-and-Collaboration-Tool-frontend/src/app/models/add-participant.spec.ts
@@ -0,0 +1,7 @@
+import { AddParticipant } from './add-participant';
+
+describe('AddParticipant', () => {
+  it('should create an instance', () => {
+    expect(new AddParticipant()).toBeTruthy();
+  });
+});
