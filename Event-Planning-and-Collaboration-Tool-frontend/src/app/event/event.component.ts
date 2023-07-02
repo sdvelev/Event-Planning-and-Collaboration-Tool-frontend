3916a3979140fb36f5e3ee4bf651f6f7259e6363
Stefan Velev <sdvelev@gmail.com>
2023-07-02 16:50:49 +0300

    fix: changed input type in form to password

--- Event-Planning-and-Collaboration-Tool-frontend/src/app/event/event.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component, OnInit} from '@angular/core';
-
-@Component({
-  selector: 'app-event',
-  templateUrl: './event.component.html',
-  styleUrls: ['./event.component.css']
-})
-export class EventComponent {}
