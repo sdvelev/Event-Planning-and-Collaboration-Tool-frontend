53444666506e70fa6ff65bb965250eee8034ba81
Stefan Velev <sdvelev@gmail.com>
2023-07-02 16:51:28 +0300

    feat: added audit fields in expense and budget models

--- Event-Planning-and-Collaboration-Tool-frontend/src/app/models/budget.ts
+++ Event-Planning-and-Collaboration-Tool-frontend/src/app/models/budget.ts
@@ -5,13 +5,21 @@ export class Budget {
   amount: number;
   show_expenses: boolean;
   event_id: number;
+  created_by: string;
+  creation_time: string;
+  updated_by: string;
+  last_updated_time: string;
 
-  constructor(id: number = 0, description: string = "", expenditure_category: string = "", amount: number = 0, show_expenses: boolean = false, event_id: number = 0) {
+  constructor(id: number = 0, description: string = "", expenditure_category: string = "", amount: number = 0, show_expenses: boolean = false, event_id: number = 0, created_by: string = "", creation_time: string = "", updated_by: string = "", last_updated_time: string = "") {
     this.id = id;
     this.description = description;
     this.expenditure_category = expenditure_category;
     this.amount = amount;
     this.show_expenses = show_expenses;
     this.event_id = event_id;
+    this.created_by = created_by;
+    this.creation_time = creation_time;
+    this.updated_by = updated_by;
+    this.last_updated_time = last_updated_time;
   }
 }
--- Event-Planning-and-Collaboration-Tool-frontend/src/app/models/expense.ts
+++ Event-Planning-and-Collaboration-Tool-frontend/src/app/models/expense.ts
@@ -3,11 +3,19 @@ export class Expense {
   description: string;
   expenditure_category: string;
   amount: number;
+  created_by: string;
+  creation_time: string;
+  updated_by: string;
+  last_updated_time: string;
 
-  constructor(id: number = 0, description: string = "", expenditure_category: string = "", amount: number = 0) {
+  constructor(id: number = 0, description: string = "", expenditure_category: string = "", amount: number = 0, created_by: string = "", creation_time: string = "", updated_by: string = "", last_updated_time: string = "") {
     this.id = id;
     this.description = description;
     this.expenditure_category = expenditure_category;
     this.amount = amount;
+    this.created_by = created_by;
+    this.creation_time = creation_time;
+    this.updated_by = updated_by;
+    this.last_updated_time = last_updated_time;
   }
 }
